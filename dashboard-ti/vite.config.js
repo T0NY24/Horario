cf651f16519dd5737cc15f4a00bf591713250db3
Anthony Perez Cuenca Bryan <116665146+T0NY24@users.noreply.github.com>
2025-12-04 00:09:34 -0500

    fix: usar rutas relativas para evitar 404

--- dashboard-ti/vite.config.js
+++ dashboard-ti/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { postgres } from 'vite-plugin-db';
 
 export default defineConfig({
-  base: '/Horario/',
+  base: './',
   plugins: [
     postgres({
       // Esto inyectará VITE_DATABASE_URL en tu .env automáticamente
