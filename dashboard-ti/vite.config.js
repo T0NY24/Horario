3d40c4903cd2a134f18bb7d6d800b7f99632fac4
Anthony Perez Cuenca Bryan <116665146+T0NY24@users.noreply.github.com>
2025-12-04 00:06:59 -0500

    fix: configurar base path para GitHub Pages

--- dashboard-ti/vite.config.js
+++ dashboard-ti/vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react';
 import { postgres } from 'vite-plugin-db';
 
 export default defineConfig({
+  base: '/Horario/',
   plugins: [
     postgres({
       // Esto inyectará VITE_DATABASE_URL en tu .env automáticamente
       envPrefix: 'VITE_',
       seed: {
         type: 'sql-script',
-        path: './init.sql', // El archivo que crearemos en el paso 4
+        path: './init.sql',
       },
     }),
     react(),
